febc11826300072539833791b25a871321610c02
Scott Chase Waggener <chase.waggener@icloud.com>
2025-07-01 00:37:55 +0000

    Fix script

--- analysis/cifar10/analyze.sh
+++ analysis/cifar10/analyze.sh
@@ -9,25 +9,25 @@ DATA=/mnt/data/cifar10/cifar-10-batches-py
 CONFIG=config/cifar10/mjepa/vit-small.yaml
 BACKBONE=output/cifar10/2025-06-29_23-44-56_mjepa-vit-s-fourier-linprobe
 
-#pdm run scripts/cifar10_visualize.py \
-#    $CONFIG \
-#    $BACKBONE/backbone.safetensors \
-#    $DATA \
-#    $OUTPUT/pca.png \
-#    -d $DEVICE -o $OFFSET -n $NUM_COMPONENTS -dt bf16 -s $SIZE
-#
-#pdm run scripts/cifar10_norms.py \
-#    $CONFIG \
-#    $BACKBONE/backbone.safetensors \
-#    $DATA \
-#    $OUTPUT/norms.png \
-#    -d $DEVICE
-#
-#pdm run scripts/position_visualize.py \
-#    $CONFIG \
-#    $BACKBONE/backbone.safetensors \
-#    $OUTPUT/positions.png \
-#    -d $DEVICE -dt bf16 
+pdm run scripts/cifar10_visualize.py \
+    $CONFIG \
+    $BACKBONE/backbone.safetensors \
+    $DATA \
+    $OUTPUT/pca.png \
+    -d $DEVICE -o $OFFSET -n $NUM_COMPONENTS -dt bf16 -s $SIZE
+
+pdm run scripts/cifar10_norms.py \
+    $CONFIG \
+    $BACKBONE/backbone.safetensors \
+    $DATA \
+    $OUTPUT/norms.png \
+    -d $DEVICE
+
+pdm run scripts/position_visualize.py \
+    $CONFIG \
+    $BACKBONE/backbone.safetensors \
+    $OUTPUT/positions.png \
+    -d $DEVICE -dt bf16 
 
 pdm run scripts/runtime_visualize.py \
     $CONFIG \
