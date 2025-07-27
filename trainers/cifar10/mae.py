3e78faf56672ded0459a70e0911b99760435d976
Scott Chase Waggener <chase.waggener@icloud.com>
2025-07-27 13:42:21 +0000

    Update MAE

--- trainers/cifar10/mae.py
+++ trainers/cifar10/mae.py
@@ -1,9 +1,11 @@
+import gc
 import math
 import os
 import warnings
 from argparse import ArgumentParser, Namespace
+from functools import partial
 from pathlib import Path
-from typing import Final, cast
+from typing import Final, Sequence, cast
 
 import safetensors.torch as st
 import torch
@@ -23,11 +25,13 @@ from torchvision.datasets import CIFAR10
 from torchvision.transforms.v2 import (
     ColorJitter,
     Compose,
+    RandomApply,
     RandomGrayscale,
     RandomHorizontalFlip,
     RandomResizedCrop,
     RandomRotation,
     RandomVerticalFlip,
+    Resize,
     ToDtype,
     ToImage,
 )
@@ -36,9 +40,11 @@ from vit import ViT, ViTConfig
 from vit.tokens import apply_mask
 
 from mjepa.jepa import CrossAttentionPredictor, generate_masks
+from mjepa.logging import CSVLogger
 from mjepa.mae import MAEConfig
 from mjepa.optimizer import OptimizerConfig
 from mjepa.trainer import (
+    DataLoaderFn,
     TrainerConfig,
     assert_all_trainable_params_have_grad,
     calculate_total_steps,
@@ -49,35 +55,30 @@ from mjepa.trainer import (
     is_rank_zero,
     rank_zero_info,
     save_checkpoint,
+    scale_change,
     setup_logdir,
     should_step_optimizer,
+    size_change,
 )
 
 
 NUM_CLASSES: Final[int] = 10
 UNKNOWN_LABEL: Final[int] = -1
 WINDOW: Final[int] = 5
+LOG_INTERVAL: Final[int] = 50
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 
-class Wrapper(nn.Module):
-    def __init__(self, backbone: ViT, predictor: nn.Module, probe: nn.Module):
-        super().__init__()
-        self.backbone = backbone
-        self.predictor = predictor
-        self.probe = probe
-
-
-def get_train_transforms() -> Compose:
+def get_train_transforms(size: Sequence[int]) -> Compose:
     return Compose(
         [
             RandomHorizontalFlip(p=0.5),
             RandomVerticalFlip(p=0.5),
-            RandomResizedCrop(size=(32, 32), scale=(0.75, 1.0), ratio=(0.75, 1.33)),
-            RandomRotation(degrees=15),
+            RandomResizedCrop(size=size, scale=(0.75, 1.0), ratio=(0.75, 1.33)),
+            RandomApply([RandomRotation(degrees=15)], p=0.25),
             ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
             RandomGrayscale(p=0.1),
             ToImage(),
@@ -86,17 +87,25 @@ def get_train_transforms() -> Compose:
     )
 
 
-def get_val_transforms() -> Compose:
+def get_val_transforms(size: Sequence[int]) -> Compose:
     return Compose(
         [
+            Resize(size=size),
             ToImage(),
             ToDtype(torch.float32, scale=True),
         ]
     )
 
 
-def get_train_dataloader(root: Path, batch_size: int, num_workers: int, local_rank: int, world_size: int) -> DataLoader:
-    transforms = get_train_transforms()
+def get_train_dataloader(
+    size: Sequence[int],
+    batch_size: int,
+    root: Path,
+    num_workers: int,
+    local_rank: int,
+    world_size: int,
+) -> DataLoader:
+    transforms = get_train_transforms(size)
     dataset = CIFAR10(root=root, train=True, transform=transforms, download=True)
     if world_size > 1:
         sampler = DistributedSampler(dataset, num_replicas=world_size, rank=local_rank, shuffle=True)
@@ -114,8 +123,15 @@ def get_train_dataloader(root: Path, batch_size: int, num_workers: int, local_ra
         )
 
 
-def get_val_dataloader(root: Path, batch_size: int, num_workers: int, local_rank: int, world_size: int) -> DataLoader:
-    transforms = get_val_transforms()
+def get_val_dataloader(
+    size: Sequence[int],
+    batch_size: int,
+    root: Path,
+    num_workers: int,
+    local_rank: int,
+    world_size: int,
+) -> DataLoader:
+    transforms = get_val_transforms(size)
     dataset = CIFAR10(root=root, train=False, transform=transforms, download=True)
     if world_size > 1:
         sampler = DistributedSampler(dataset, num_replicas=world_size, rank=local_rank, shuffle=True)
@@ -135,8 +151,8 @@ def get_val_dataloader(root: Path, batch_size: int, num_workers: int, local_rank
 
 def train(
     modules: nn.ModuleDict,
-    train_dataloader: DataLoader,
-    val_dataloader: DataLoader,
+    train_dataloader_fn: DataLoaderFn,
+    val_dataloader_fn: DataLoaderFn,
     optimizer: Optimizer,
     scheduler: LRScheduler,
     mae_config: MAEConfig,
@@ -151,12 +167,16 @@ def train(
     predictor: CrossAttentionPredictor = modules["predictor"]
     rank_zero_info(f"Backbone params: {format_large_number(count_parameters(backbone))}")
     rank_zero_info(f"Predictor params: {format_large_number(count_parameters(predictor))}")
+    mae_scale = mae_config.scale
 
+    # DataLoader setup
+    train_dataloader = train_dataloader_fn(backbone.config.img_size, trainer_config.batch_size)
+    val_dataloader = val_dataloader_fn(backbone.config.img_size, trainer_config.batch_size)
+
+    accumulate_grad_batches = trainer_config.accumulate_grad_batches
     microbatch = (last_epoch + 1) * len(train_dataloader)
-    step = microbatch // trainer_config.accumulate_grad_batches
-    total_steps = calculate_total_steps(
-        train_dataloader, trainer_config.num_epochs, trainer_config.accumulate_grad_batches
-    )
+    step = microbatch // accumulate_grad_batches
+    total_steps = calculate_total_steps(train_dataloader, trainer_config.num_epochs, accumulate_grad_batches)
     rank_zero_info(f"Training for {trainer_config.num_epochs} epochs = {total_steps} steps")
     rank_zero_info(
         f"Batch size: {trainer_config.batch_size}, Microbatch accumulation: {trainer_config.accumulate_grad_batches}"
@@ -166,10 +186,28 @@ def train(
     train_loss = tm.RunningMean(window=WINDOW).cuda()
     train_acc = Running(tm.Accuracy(task="multiclass", num_classes=NUM_CLASSES), window=WINDOW).cuda()
     val_acc = tm.Accuracy(task="multiclass", num_classes=NUM_CLASSES).cuda()
+    logger = CSVLogger(
+        log_dir / "run.csv", interval=LOG_INTERVAL, accumulate_grad_batches=trainer_config.accumulate_grad_batches
+    )
 
     img: Tensor
     label: Tensor
     for epoch in range(last_epoch + 1, trainer_config.num_epochs):
+        # Update training resolution / batch_size / accumulate_grad_batches if necessary
+        if trainer_config.is_size_change_epoch(epoch):
+            size_config = trainer_config.sizes[epoch]
+            train_dataloader, val_dataloader, accumulate_grad_batches = size_change(
+                size_config,
+                trainer_config.batch_size,
+                accumulate_grad_batches,
+                train_dataloader_fn,
+                val_dataloader_fn,
+            )
+            mae_scale = scale_change(backbone.config.img_size, size_config, mae_scale)
+            rank_zero_info(
+                f"Changing size to {size_config.size} and batch size to {size_config.batch_size} (accumulate grad batches: {accumulate_grad_batches})"
+            )
+
         modules.train()
         desc = format_pbar_description(step, microbatch, epoch, loss=train_loss, acc=train_acc)
         pbar = tqdm(train_dataloader, desc=desc, disable=not is_rank_zero(), leave=False)
@@ -212,8 +250,9 @@ def train(
             microbatch += 1
 
             # Optimizer update and teacher update
-            if should_step_optimizer(microbatch, trainer_config.accumulate_grad_batches):
-                scheduler.step()
+            if should_step_optimizer(microbatch, accumulate_grad_batches):
+                if step < total_steps:
+                    scheduler.step()
                 optimizer.step()
                 optimizer.zero_grad()
                 step += 1
@@ -236,6 +275,11 @@ def train(
 
             # Validation epoch end
             rank_zero_info(f"Epoch: {epoch}, Val Acc: {val_acc.compute():.4f}")
+            logger.log(epoch, step, microbatch, acc=val_acc.compute())
+
+        gc.collect()
+        torch.cuda.synchronize()
+        torch.cuda.empty_cache()
 
         # Save checkpoint
         if log_dir:
@@ -316,12 +360,21 @@ def main(args: Namespace) -> None:
         wrapper = DDP(wrapper, device_ids=[local_rank])
 
     # Instantiate dataloaders
-    train_dataloader = get_train_dataloader(
-        args.data, trainer_config.batch_size, trainer_config.num_workers, local_rank, world_size
+    train_dataloader_fn = partial(
+        get_train_dataloader,
+        root=args.data,
+        num_workers=trainer_config.num_workers,
+        local_rank=local_rank,
+        world_size=world_size,
     )
-    val_dataloader = get_val_dataloader(
-        args.data, trainer_config.batch_size, trainer_config.num_workers, local_rank, world_size
+    val_dataloader_fn = partial(
+        get_val_dataloader,
+        root=args.data,
+        num_workers=trainer_config.num_workers,
+        local_rank=local_rank,
+        world_size=world_size,
     )
+    train_dataloader = train_dataloader_fn(backbone.config.img_size, trainer_config.batch_size)
 
     # Instantiate optimizer and scheduler
     total_steps = calculate_total_steps(
@@ -336,7 +389,7 @@ def main(args: Namespace) -> None:
         log_dir = None
 
     ignore_warnings()
-    train(wrapper, train_dataloader, val_dataloader, optimizer, scheduler, mae_config, trainer_config, log_dir)
+    train(wrapper, train_dataloader_fn, val_dataloader_fn, optimizer, scheduler, mae_config, trainer_config, log_dir)
 
 
 def entrypoint() -> None:
